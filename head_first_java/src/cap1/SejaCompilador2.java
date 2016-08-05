af57b56e42045a8a844e9dcc886bc8218e828be9
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2016-08-05 15:21:18 -0300

    cap 1

---
+++ head_first_java/src/cap1/SejaCompilador2.java
@@ -0,0 +1,18 @@
+package cap1;
+
+public class SejaCompilador2 {
+
+	//compila normal
+	public static void main(String[] args) {
+		
+		int x = 5;
+		while(x > 1){
+			x = x-1;
+			if(x<3){
+				System.out.println("small x");
+			}
+		}
+		
+	}
+	
+}
